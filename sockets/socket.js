7df24b561882b96268596b4dd6ad3fedc6ad2706
des_web_05 <des_web_05@cobama.com.mx>
2021-12-01 18:48:57 -0600

    Server actual

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,3 +1,4 @@
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
 
@@ -5,11 +6,16 @@ const { io } = require('../index');
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+    
     const [valido,uid]= comprobarJWT(client.handshake.headers['x-token']);
     
+    //Verificar Autentificacion
     if (!valido) {return client.disconnect();}
+    //Cliente autenticado
+    usuarioConectado(uid);
     console.log('Cliente autenticado');
     client.on('disconnect', () => {
+        usuarioDesconectado(uid);
         console.log('Cliente desconectado');
     });
 });
